b3a82bca2eee3c084eb094d169db04c6430ae0c8
francissean024@gmail.com <francissean024@gmail.com>
2026-06-23 14:25:48 +1000

    Create main.py

---
+++ labyrinth/main.py
@@ -0,0 +1,11 @@
+#assesment_task_2 
+#dungeon_crawler element for project_1 (Bees Knees Tub Repairs)
+#standalone and compatability features for project_1
+
+#welcome statement for game
+def welcome():
+    #intro
+    print("produceed by the Bees Knees Tub Repairs")
+    #title screen
+    print("Welcome to the Labyrinth!")
+    #fade to main menu GUI or equivalent
